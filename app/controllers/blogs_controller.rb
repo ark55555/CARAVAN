14212943f2054dadbe60c84ff8328067e5f78b0f
ark55555 <ark.tooyoung@gmail.com>
2021-04-12 10:26:47 +0000

    [Finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -30,7 +30,7 @@ class BlogsController < ApplicationController
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
-    redirect_to blogs
+    redirect_to blogs_path
   end
   
   private
